45d37d34633276696f7743b7a3cdff93cc74dba7
kessystaub <51220952+kessystaub@users.noreply.github.com>
2022-03-30 10:56:28 -0300

    Update Nota.java

--- questao4/Nota.java
+++ questao4/Nota.java
@@ -1,13 +1,13 @@
-package questao4;
-
 public class Nota{
     private String nome_aluno;
     private String nome_disciplina;
-    private double nota_alcancada;
+    public double[] notas = new double[2];
 
-    public Nota(String nome, String disc, double nota){
+    public Nota(String nome, String disc,double nota_semestre1,double nota_semestre2){
         this.nome_aluno = nome;
         this.nome_disciplina = disc;
-        this.nota_alcancada = nota;
+        this.notas[0]=nota_semestre1;
+        this.notas[1]=nota_semestre2;
     }
+
 }
