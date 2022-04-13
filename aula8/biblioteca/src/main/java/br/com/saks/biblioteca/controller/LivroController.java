7cf4a160f0ffaee914ffd93841b63b8ba354216a
kessystaub <51220952+kessystaub@users.noreply.github.com>
2022-04-13 17:16:27 -0300

    Add files via upload

--- aula8/biblioteca/src/main/java/br/com/saks/biblioteca/controller/LivroController.java
+++ aula8/biblioteca/src/main/java/br/com/saks/biblioteca/controller/LivroController.java
@@ -6,9 +6,12 @@ import br.com.saks.biblioteca.repository.LivroRepository;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -34,6 +37,25 @@ public class LivroController {
     @PostMapping
     public Livro adicionar(@RequestBody Livro livro){
         return livroRepository.save(livro);
+    }
+    
+    @PutMapping(value="/(id)")
+    public ResponseEntity editar(@PathVariable Long id, @RequestBody Livro livro){
+        return livroRepository.findById(id)
+        .map(record->{
+            record.setTitulo(livro.getTitulo());
+            Livro livroUpdated = livroRepository.save(record);
+            return ResponseEntity.ok().body(livroUpdated);
+        }).orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping(value="/(id)")
+    public ResponseEntity deletar(@PathVariable Long id){
+        return livroRepository.findById(id)
+        .map(record->{
+            livroRepository.deleteById(id);
+            return ResponseEntity.ok().build();
+        }).orElse(ResponseEntity.notFound().build());
     }
     
 }
